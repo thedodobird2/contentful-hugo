330498697510e4b0f01c176d5396c09c07d0b424
Hudson Muff <hudson.muff@unic.com>
2018-03-22 13:02:39 +0100

    WEBE-297: implemented simple request to get all entries in a contentful space

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/thedodobird2/contentful-hugo/contentful"
+)
+
+const (
+	// Content Delivery API
+	cdaUrl   = ""
+	cdaToken = ""
+	// Content Preview API
+	cpaBaseUrl = ""
+	cpaToken   = ""
+	spaceId = ""
+)
 
 func main() {
 	fmt.Println("Contentful, meet Hugo")
+
+	space := &contentful.Space{
+		spaceId,
+		cdaToken,
+	}
+
+	fmt.Println(string(space.GetEntries()))
 }
