cf768fb44f1044ee9d813d122750ef300dcb6889
Hudson Muff <hudson.muff@unic.com>
2018-03-23 13:35:50 +0100

    WEBE-301: fixed test

--- config/config_test.go
+++ config/config_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestGet(t *testing.T) {
 	mockConfig := Config{}
-	mockConfig.Contentful.Access_token = "mockTasdoken"
+	mockConfig.Contentful.Access_token = "mockToken"
 	mockConfig.Contentful.Space_id = "mockSpaceId"
 
 	if Get("testcontent/mockConfig.json") != mockConfig {
